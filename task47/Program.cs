39a03a09071a7f21353c2d5b5f4fb833c666c7bd
dvvinokurov <dmitvitvin@gmail.com>
2022-05-14 23:22:31 +0300

    add task47

---
+++ task47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив m x n заполненный случайными вещественными числами
+int[] parametersArray = new int[3];
+string[] partsMessage = {"строк ", "столбцов ", "знаков после запятой "};
+for(int i = 0; i < parametersArray.Length; i++)
+{
+    Console.Write("Введите количество " + partsMessage[i]);
+    parametersArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+double[,] array = new double[parametersArray[0], parametersArray[1]];
+
+void FillRandomDoubleArray(double[,] array)
+{
+    var r = new Random();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            int factor = r.Next(-20, 21);
+            double numDouble = r.NextDouble();
+            array[i, j] = Math.Round(factor * numDouble, parametersArray[2]);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillRandomDoubleArray(array);
+PrintArray(array);
+
